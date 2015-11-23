87aca13af777568e5ba19e4417c27a10a12f2ebc
Lululuz <hamalaa@web.de>
2015-11-23 17:59:52 +0100

    Add Log Class

---
+++ FutBud/Services/WriteLog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FutBud.Services
+{
+    static class WriteLog
+    {
+        public static void DoWrite(string str)
+        {
+            var log = !File.Exists("log.txt") ? new StreamWriter("log.txt") : File.AppendText("log.txt");
+            log.WriteLine(DateTime.Now + " # " + str);
+            log.Close();
+        }
+    }
+    
+}
